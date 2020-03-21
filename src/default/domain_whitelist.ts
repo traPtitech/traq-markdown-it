8a7722ecb07a4971e75353de57207a7f5745153f
sapphi-red <green@sapphi.red>
2020-03-21 17:56:34 +0900

    add domain to domain_whitelist

--- src/default/domain_whitelist.ts
+++ src/default/domain_whitelist.ts
@@ -3,7 +3,9 @@ export default [
   'user-images.githubusercontent.com',
   'git.trap.jp',
   'wiki.trapti.tech',
+  'wiki.trap.jp',
   'md.trapti.tech',
+  'md.trap.jp',
   'trap.jp',
   'traq-dev.tokyotech.org'
 ]
