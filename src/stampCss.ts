91c97c826b482a7d042d4ccaeb8f388aa97185d5
sapphi-red <green@sapphi.red>
2020-08-07 22:36:31 +0900

    stampCssPluginでサイズ指定なしに対応

--- src/stampCss.ts
+++ src/stampCss.ts
@@ -2,33 +2,37 @@ import type MarkdownIt from 'markdown-it'
 import regexp from 'markdown-it-regexp'
 
 // See stamp.ts stampRegExp
-const stampRegExp = /:((?:[a-zA-Z0-9+_-]{1,32}|@[a-zA-Z0-9_-]+)[\w+-.]*)[:;]/
+const stampRegExp = /:((?:[a-zA-Z0-9+_-]{1,32}|@[a-zA-Z0-9_-]+)[\w+-.]*):/
+const stampRegExpWithSize = /:((?:[a-zA-Z0-9+_-]{1,32}|@[a-zA-Z0-9_-]+)[\w+-.]*)[:;]/
 
-const renderUserStamp = (wrappedName: string, name: string) => {
+const renderUserStamp = (wrappedName: string, name: string, size: number) => {
   // 最初の@を取り除く
   const username = name.slice(1)
 
-  return `<i class="emoji s${
-    wrappedName.endsWith(':') ? 32 : 16
-  }" title="${wrappedName}" style="background-image: url(https://q.trap.jp/api/v3/public/icon/${username})">${wrappedName}</i>`
+  return `<i class="emoji s${size}" title="${wrappedName}" style="background-image: url(https://q.trap.jp/api/v3/public/icon/${username})">${wrappedName}</i>`
 }
 
-const renderStamp = (wrappedName: string, name: string) => {
-  const size = wrappedName.endsWith(':') ? 32 : 16
+const renderStamp = (wrappedName: string, name: string, size: number) => {
   const className = `e_${name.replace(/\+/g, '_-plus-_')}`
   return `<i class="emoji s${size} ${className}" title="${wrappedName}">${wrappedName}</i>`
 }
 
 export const stampCssPlugin = (
   md: MarkdownIt,
-  stamps: readonly string[]
+  stamps: readonly string[],
+  enableSize = false
 ): void => {
-  regexp(stampRegExp, ([wrappedName, name]): string => {
-    if (name.startsWith('@')) {
-      return renderUserStamp(wrappedName, name)
-    }
+  regexp(
+    enableSize ? stampRegExpWithSize : stampRegExp,
+    ([wrappedName, name]): string => {
+      const size = enableSize ? (wrappedName.endsWith(':') ? 32 : 16) : 24
 
-    if (!stamps.includes(name)) return wrappedName
-    return renderStamp(wrappedName, name)
-  })(md)
+      if (name.startsWith('@')) {
+        return renderUserStamp(wrappedName, name, size)
+      }
+
+      if (!stamps.includes(name)) return wrappedName
+      return renderStamp(wrappedName, name, size)
+    }
+  )(md)
 }
--- tests/stampCss.test.ts
+++ tests/stampCss.test.ts
@@ -4,12 +4,19 @@ import { stampCssPlugin } from '#/stampCss'
 const setup = () => {
   const md = new MarkdownIt()
 
+  stampCssPlugin(md, ['xx', 'po', 'xx+po+xx'], true)
+  return md
+}
+const setupSizeDisabled = () => {
+  const md = new MarkdownIt()
+
   stampCssPlugin(md, ['xx', 'po', 'xx+po+xx'])
   return md
 }
 
 describe('container', () => {
   const md = setup()
+  const mdSizeDisabled = setupSizeDisabled()
 
   it('can render stamp (1)', () => {
     const actual = md
@@ -44,6 +51,17 @@ describe('container', () => {
     const expected = '<p>:invalid:</p>'
     expect(actual).toBe(expected)
   })
+  it('can render stamp with size disabled', () => {
+    const actual = mdSizeDisabled
+      .render(
+        `
+:xx:
+    `
+      )
+      .trim()
+    const expected = '<p><i class="emoji s24 e_xx" title=":xx:">:xx:</i></p>'
+    expect(actual).toBe(expected)
+  })
 
   it('can render user stamp (1)', () => {
     const actual = md
@@ -66,6 +84,18 @@ describe('container', () => {
       )
       .trim()
     const expected = '<p>:@Webhook#user:</p>'
+    expect(actual).toBe(expected)
+  })
+  it('can render user stamp with size disabled', () => {
+    const actual = mdSizeDisabled
+      .render(
+        `
+:@user:
+    `
+      )
+      .trim()
+    const expected =
+      '<p><i class="emoji s24" title=":@user:" style="background-image: url(https://q.trap.jp/api/v3/public/icon/user)">:@user:</i></p>'
     expect(actual).toBe(expected)
   })
 })
