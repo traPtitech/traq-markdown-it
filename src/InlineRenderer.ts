dd2d50caa90590dbef864154f247091831886219
sapphi-red <green@sapphi.red>
2021-04-09 12:54:22 +0900

    InlineRendererの実装
    fix #59
    close #93

---
+++ src/InlineRenderer.ts
@@ -0,0 +1,81 @@
+import type { Options } from 'markdown-it'
+import /* tree-shaking no-side-effects-when-called */ Renderer, {
+  RenderRuleRecord
+} from 'markdown-it/lib/renderer'
+import type Token from 'markdown-it/lib/token'
+import { escapeHtml } from './util'
+
+const formatCodeBlockContent = (code: string) =>
+  escapeHtml(code.replace(/\n$/, '').replace(/\n/g, ' '))
+
+export class InlineRenderer extends Renderer {
+  /**
+   * Rulesのblockルールは使われない
+   * blockRulesに入れたもののみ利用される
+   */
+  private blockRules: RenderRuleRecord = {}
+
+  setRules(defaultRules: RenderRuleRecord | null = null): void {
+    if (defaultRules !== null) {
+      this.rules = { ...defaultRules }
+    }
+
+    /* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+    this.rules.softbreak = () => ' '
+    this.rules.hardbreak = () => ' '
+    this.rules.image = (tokens, idx) => {
+      const token = tokens[idx]!
+      const attrsStr = (token.attrs ?? [])
+        .filter(([attr]) => attr !== 'src' && attr !== 'alt')
+        .map(([key, val]) => `${escapeHtml(key)}="${escapeHtml(val)}"`)
+        .join(' ')
+      return `<a href="${
+        token.attrGet('src') ?? ''
+      }" ${attrsStr} data-is-image>${token.content}</a>`
+    }
+
+    this.blockRules.code_block = (tokens, idx) =>
+      '```' + formatCodeBlockContent(tokens[idx]!.content) + '```'
+    this.blockRules.fence = (tokens, idx) => {
+      const { markup, content } = tokens[idx]!
+      return `${markup}${formatCodeBlockContent(content)}${markup}`
+    }
+    this.blockRules.hr = (tokens, idx) => ` ${tokens[idx]!.markup} `
+    this.blockRules.heading_open = (tokens, idx) =>
+      `${'#'.repeat(+tokens[idx]!.tag.slice(1))} `
+    this.blockRules.blockquote_open = (tokens, idx) => `${tokens[idx]!.markup} `
+    this.blockRules.list_item_open = (tokens, idx) => `${tokens[idx]!.markup} `
+
+    /* eslint-enable @typescript-eslint/no-non-null-assertion */
+  }
+
+  renderContent(token: Token): string {
+    return escapeHtml(token.content)
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+  render(tokens: Token[], options: Options, env: any): string {
+    let result = ''
+
+    for (const [i, token] of tokens.entries()) {
+      const type = token.type
+      const blockRule = this.blockRules[type]
+
+      // 意味の切れ目に空白を入れる
+      if (token.nesting === 1 && tokens[i - 1]?.nesting === -1) {
+        result += ' '
+      }
+
+      if (type === 'inline') {
+        result += this.renderInline(token.children ?? [], options, env)
+      } else if (blockRule !== undefined) {
+        result += blockRule(tokens, i, options, env, this)
+      } else {
+        result += this.renderContent(token)
+      }
+    }
+
+    return result
+  }
+}
---
+++ tests/InlineRenderer.test.ts
@@ -0,0 +1,136 @@
+import { InlineRenderer } from '#/InlineRenderer'
+import MarkdownIt from 'markdown-it'
+import dedent from 'ts-dedent'
+
+describe('InlineRenderer', () => {
+  const md = new MarkdownIt()
+  const ir = new InlineRenderer()
+  ir.setRules()
+
+  const render = (input: string) => {
+    const tokens = md.parse(input, {})
+    return ir.render(tokens, md.options, {})
+  }
+
+  it('can render codeblock', () => {
+    const actual = render(
+      `
+    code
+    a
+`
+    )
+    const expected = '```code a```'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render fenced codeblock', () => {
+    const actual = render(dedent`
+      \`\`\`js
+      code
+      a
+      \`\`\`
+    `)
+    const expected = '```code a```'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render hr', () => {
+    const actual = render(dedent`
+      ---
+      ***
+    `)
+    const expected = ' ---  *** '
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render header', () => {
+    const actual = render(dedent`
+      # h1
+      ## h2
+    `)
+    const expected = '# h1 ## h2'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render ruled header', () => {
+    const actual = render(dedent`
+      h1
+      =
+      h2
+      h2
+      -
+    `)
+    const expected = '# h1 ## h2 h2'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render defined link', () => {
+    const actual = render(dedent`
+      [link]:url 'title'
+
+      [link]
+    `)
+    const expected = '<a href="url" title="title">link</a>'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render paragraph', () => {
+    const actual = render(dedent`
+      p1
+
+      p2
+
+      # h1
+    `)
+    const expected = 'p1 p2 # h1'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render blockquote', () => {
+    const actual = render(dedent`
+      > aaa
+      > bbb
+      ccc
+    `)
+    const expected = '> aaa bbb ccc'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render unordered list', () => {
+    const actual = render(dedent`
+      - a1
+      - a2
+      a2
+      + b1
+        + b2
+      * c
+    `)
+    const expected = '- a1 - a2 a2 + b1 + b2 * c'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render ordered list', () => {
+    const actual = render(dedent`
+      1. a1
+      2. a2
+      a2
+      1) b
+      5. c
+    `)
+    // FIXME: https://developer.mozilla.org/ja/docs/Web/CSS/CSS_Lists_and_Counters/Using_CSS_counters#example_of_a_nested_counter
+    const expected = '. a1 . a2 a2 ) b . c'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render inline elements', () => {
+    const actual = render(dedent`
+      *i* _i_
+      **b** __b__
+      [link](url 'title')
+      <http://example.com>
+    `)
+    const expected =
+      '<em>i</em> <em>i</em> ' +
+      '<strong>b</strong> <strong>b</strong> ' +
+      '<a href="url" title="title">link</a> ' +
+      '<a href="http://example.com">http://example.com</a>'
+    expect(actual).toStrictEqual(expected)
+  })
+  it('can render image', () => {
+    const actual = render(dedent`
+      ![image](url 'title')
+    `)
+    const expected = '<a href="url" title="title" data-is-image>image</a>'
+    expect(actual).toStrictEqual(expected)
+  })
+})
